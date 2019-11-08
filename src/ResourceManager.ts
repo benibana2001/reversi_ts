163db0cab399c1890eb13f5700ef8fa077f74b0a
benibana2001 <benibana2001@gmail.com>
2019-11-08 14:41:54 +0900

    implement function loading image resource

--- src/ResourceManager.ts
+++ src/ResourceManager.ts
@@ -1,3 +1,21 @@
 export default class ResourceManager {
+    public imgs: ResourceImage[] = []
+    public load = (name: string, url: string): Promise<boolean> => {
+        let func = (resolve: Function, reject: Function) => {
+            let img: HTMLImageElement = new Image()
+            let newResource: ResourceImage = { name: img }
+            this.imgs.push(newResource)
+            img.onload = (() => {
+                let msg: string = `DONE Loading: ${name}`
+                console.log(msg)
+                resolve(true)
+            })
+            img.src = url
+        }
+        return new Promise(func)
+    }
+}
 
+interface ResourceImage {
+    name: HTMLImageElement
 }
