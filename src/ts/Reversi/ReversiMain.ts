8a168d2de977ed5fe4679b8cb77caa2b118f0ecf
benibana2001 <benibana2001@gmail.com>
2019-11-20 09:01:48 +0900

    implement getEnableSquares() which return all squres could be put token.

--- src/ts/Reversi/ReversiMain.ts
+++ src/ts/Reversi/ReversiMain.ts
@@ -23,78 +23,110 @@ export default class ReversiMain {
     /**
      * init
      */
-    public init ():  void {
-    
+    public init(): void {
+        // To Blank All Squares
+        this.scanBoard((i: number) => {
+            this.board[i] = this.BLANK
+        })
+        // Set Token x 4 (prepare for game start)
+        this.board[this.XYToI(3, 3)] = this.board[this.XYToI(4, 4)] = 0
+        this.board[this.XYToI(3, 4)] = this.board[this.XYToI(4, 3)] = 0
+
+        // Decide Geme Over
     }
 
     /**
      * XYToI
-     * XY座標から要素の位置を求める
+     * XY座標から要素の(配列表記上の)位置を求める
      */
-    public XYToI ():  void {
-    
+    public XYToI(x: number, y: number): number {
+        if (x < 0 || y < 0) return
+        if (x >= this.w || y >= this.h) return
+        return (x + y * this.w) | 0
     }
 
     /**
      * calcScore
      */
-    public calcScore ():  void {
-    
+    public calcScore(): void {
+
     }
 
     /**
      * scanLine
      * 1列走査
      */
-    public scanLine ():  void {
-    
+    public scanLine(): void {
+
     }
 
     /**
      * getEnableSquares
      * 配置可能マス配列の取得
+     * Return All Squares Info-Array Which Could Be Put Token
      */
-    public getEnableSquares ():  void {
-    
+    public getEnableSquares(board: number[], player: number): void {
+        let res: { x: number, y: number }[] = []
+        let playerEnemy: number = 1 - player
+        // Check All Squares
+        this.scanBoard((i: number, x: number, y: number) => {
+            // If Already Set Token, Don't Do Anything
+            if (board[i] !== this.BLANK) return
+            // About Each Square, If Can Put Token Return Square Info.
+            let l: number = this.DIRECTION.length
+            for (let j = 0; j < l; j++) {
+                let line: { patern: string, ary: { x: number, y: number }[] } = this.scanLine(board, x, y, this.DIRECTION[j].x, this.DIRECTION[j].y)
+                let re: RegExp = new RegExp("^" + playerEnemy + "+" + player)
+                if (line.patern.match(re)) {
+                    res.push({ x: x, y: y })
+                    return
+                }
+            }
+
+        }
+        )
     }
 
     /**
      * putToken
      */
-    public putToken ():  void {
-    
+    public putToken(): void {
+
     }
 
     /**
      * next
      * 進行処理
      */
-    public next ():  void {
-    
+    public next(): void {
+
     }
 
     /**
      * execReverse
      */
-    public execReverse ():  void {
-    
+    public execReverse(): void {
+
     }
 
     /**
      * skip
      */
-    public skip ():  void {
-    
+    public skip(): void {
+
     }
 
     /**
      * getPlayerType
      */
-    public getPlayerType ():  void {
-    
-    }
+    public getPlayerType(): void {
 
-    public scanBoard (f: Function): void {
+    }
+    /**
+     * getPlayerType
+     * Execute Function to All Squares
+     */
+    public scanBoard(f: Function): void {
         let max: number = this.w * this.h
         for (let i = 0; i < max; i++) {
             let x = i % this.w
